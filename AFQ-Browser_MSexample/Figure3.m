205484b7cc3679092d8701c290ca5f3c3a6915c1
jyeatman <jyeatman@stanford.edu>
2017-07-04 11:30:05 -0700

    updated figs

--- AFQ-Browser_MSexample/Figure3.m
+++ AFQ-Browser_MSexample/Figure3.m
@@ -1,65 +1,70 @@
 load AFQ-Browser_MSexample.mat
 % Script for figure 1
-tracts = [3 9 19];
+tracts = [3 9 13 19];
 fgnames = AFQ_get(afq, 'fgnames');
 ms = AFQ_get(afq, 'metadata','MultipleSclerosis');
 nodes = 6:95;
 colors = [1 0 0; 0 0 1];
 sub = 21;
 close all
-
+properties = {'md' 'rd' 'fa'}
 %% MD plot
-for ii = tracts
-    data = AFQ_get(afq, fgnames{ii}, 'md');
-    figure; hold
-    for jj = 1:2
-    d = data(ms==jj-1,nodes);
-    m = nanmean(d);
-    sd = nanstd(d);
-    n = sum(ms==0);
-    se = sd./sqrt(n);
-    % SD
-    patch([nodes fliplr(nodes)], [m+sd fliplr(m-sd)],colors(jj,:), ...
-        'edgealpha', 0, 'facealpha', .3);
-    % Plot
-    plot(nodes, m, '-', 'color', colors(jj,:), 'linewidth',3);
-    %axis([5 95 .55 .75])
+for p = 1:length(properties)
+    for ii = tracts
+        data = AFQ_get(afq, fgnames{ii}, properties{p});
+        % Plot individual subjects and SDs
+        figure; hold
+        for jj = 1:2
+            d = data(ms==jj-1,nodes);
+            m = nanmean(d);
+            sd = nanstd(d);
+            n = sum(ms==0);
+            se = sd./sqrt(n);
+            % SD
+            patch([nodes fliplr(nodes)], [m+sd fliplr(m-sd)],colors(jj,:), ...
+                'edgealpha', 0, 'facealpha', .3);
+            % Plot
+            plot(nodes, m, '-', 'color', colors(jj,:), 'linewidth',3);
+            %axis([5 95 .55 .75])
+            if jj==1
+            zs = minmax((data(sub,nodes)-m)./sd);
+            fprintf('patient max %.2f min %.2f zscore prop %s tract %s', zs(1),zs(2),properties{p},fgnames{ii})
+            end
+        end
+        % Plot patients as light lines
+        plot(repmat(nodes,[sum(ms==1),1])',data(ms==1,nodes)','-', 'color', [.5 .5 1],'linewidth',.5)
+        % Highlight one patient as a bold line
+        plot(nodes, data(sub,nodes),'-', 'color', colors(2,:),'linewidth',2)
+        % Format axis and save figure
+        hold off
+        xlabel('Distance Along Fiber Bundle','fontsize',14)
+        set(gca,'fontsize',12)
+        print(sprintf('MSpatient-Tract%d-%s.pdf',ii,properties{p}),'-dpdf')
+        close
+        
+        % Plot group comparison and SEs
+        figure; hold
+        for jj = 1:2
+            d = data(ms==jj-1,nodes);
+            m = nanmean(d);
+            sd = nanstd(d);
+            n = sum(ms==0);
+            se = sd./sqrt(n);
+            % SD
+            patch([nodes fliplr(nodes)], [m+se fliplr(m-se)],colors(jj,:), ...
+                'edgealpha', 0, 'facealpha', .3);
+            % Plot
+            plot(nodes, m, '-', 'color', colors(jj,:), 'linewidth',3);
+            %axis([5 95 .55 .75])
+        end
+        % Format axis and save figure
+        hold off
+        xlabel('Distance Along Fiber Bundle','fontsize',14)
+        set(gca,'fontsize',12)
+        print(sprintf('GroupComp-MSpatient-Tract%d-%s.pdf',ii,properties{p}),'-dpdf')
+        close
     end
-    % Plot patient
-    plot(data(sub,nodes),'-', 'color', colors(2,:),'linewidth',1)
-    
-    hold off
-    ylabel('Mean Diffusivity (\mum^2/ms)','fontsize',14)
-    xlabel('Distance Along Fiber Bundle','fontsize',14)
-    set(gca,'fontsize',12)
-    print(sprintf('Figure1_MD-%d.pdf',ii),'-dpdf')
 end
-
-%% FA plot
-for ii = tracts
-    data = AFQ_get(afq, fgnames{ii}, 'fa');
-    figure(ii); hold
-    for jj = 1:3
-        g = age>agebin(jj) & age<=agebin(jj+1);
-        d = data(g,nodes);
-        m = nanmean(d);
-        sd = nanstd(d);
-        n = sum(g);
-        se = sd./sqrt(n);
-        % errorbars
-        patch([nodes fliplr(nodes)], [m+se fliplr(m-se)],colors(jj,:), ...
-            'edgealpha', 0, 'facealpha', .3);
-        % Plot
-        plot(nodes, m, '-', 'color', colors(jj,:), 'linewidth',3);
-    end
-    axis([6 95 .35 .695])
-    hold off
-    ylabel('Fractional Anisotropy','fontsize',14)
-    xlabel('Distance Along Fiber Bundle','fontsize',14)
-    set(gca,'fontsize',12)
-    print(sprintf('Figure1_FA-%d.pdf',ii),'-dpdf')
-end
-
 
 %% Render fibers
 fg = fgRead('exampleFibers.mat')
