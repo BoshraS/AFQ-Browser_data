f4360f511445fb34675370f0284fb3d9748fd136
jyeatman <jyeatman@stanford.edu>
2017-07-05 21:33:47 -0700

    updated to function

--- Tableu20_Fibers.m
+++ Tableu20_Fibers.m
@@ -2,8 +2,7 @@
 cd ~/git/AFQ-Browser_data/AFQ-Browser_example
 load AFQ-Browser_example.mat
 fg = fgRead('exampleFibers.mat')
-rgb = [31,119,180;174,199,232;255,127,14;255,187,120;44,160,44;152,223,138;214,39,40;255,152,150;148,103,189;197,176,213
-140,86,75;196,156,148;227,119,194;247,182,210;127,127,127;199,199,199;188,189,34;219,219,141;23,190,207;158,218,229];
+rgb = tableu20;
 fgnames = AFQ_get(afq,'fgnames')
 for ii = 1:20
    lh = AFQ_RenderFibers(fg(ii),'numfibers',500,'color',rgb(ii,:)./255);
